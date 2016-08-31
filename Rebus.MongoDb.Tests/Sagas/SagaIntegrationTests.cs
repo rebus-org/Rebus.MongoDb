7bd027609470f57e88c9581e8d2f30e348e31a98
Mogens Heller Grabe <mookid8000@gmail.com>
2016-08-31 10:55:57 +0200

    now you are here

---
+++ Rebus.MongoDb.Tests/Sagas/SagaIntegrationTests.cs
@@ -0,0 +1,8 @@
+﻿using NUnit.Framework;
+using Rebus.Tests.Contracts.Sagas;
+
+namespace Rebus.MongoDb.Tests.Sagas
+{
+    [TestFixture, Category(MongoTestHelper.TestCategory)]
+    public class SagaIntegrationTests : SagaIntegrationTests<TestMongoDbSagaStorage> { }
+}
